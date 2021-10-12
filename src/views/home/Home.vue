ae2ae7ed5ba14b09531b4dc138e28f476b0d32bb
TakheiChan <694813200@qq.com>
2021-10-12 20:00:54 +0800

    test

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,10 +1,19 @@
 <template>
-  <h1>首页</h1>
+
+  <div id="home">
+    <h1>首页</h1>
+    <nav-bar><div slot="center">购物街</div> </nav-bar>
+  </div>
 </template>
 
 <script>
+  import NavBar from "../../components/common/navbar/NavBar";
+
   export default {
-    name: "Home"
+    name: "Home",
+    components: {
+      NavBar
+    }
   }
 </script>
 
