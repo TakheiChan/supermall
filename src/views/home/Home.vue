b83fbdf18382bfcb19c88d96409bfe266488f17d
TakheiChan <694813200@qq.com>
2021-10-20 12:43:48 +0800

    test

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -5,14 +5,20 @@
     <nav-bar class="home-nav"><div slot="center">购物街</div> </nav-bar>
     <home-swiper :banners="banners"></home-swiper>
     <recommend-view :recommends="recommends"></recommend-view>
+    <feature-view></feature-view>
+    <tab-control class="tab-control" :titles="['流行','新款','精选']"></tab-control>
   </div>
 </template>
 
 <script>
   import NavBar from "../../components/common/navbar/NavBar";
+  import tabControl from "../../components/content/tabControl/tabControl";
+
   import {getHomeMultidata} from "../../network/home";
+
   import HomeSwiper from "./childComps/HomeSwiper";
   import RecommendView from "./childComps/RecommendView";
+  import FeatureView from "./childComps/FeatureView";
   // import Swiper from "../../components/common/swiper/Swiper";
 
 
@@ -21,7 +27,9 @@
     components: {
       HomeSwiper,
       NavBar,
-      RecommendView
+      RecommendView,
+      FeatureView,
+      tabControl
     },
     data() {
       return {
@@ -41,8 +49,20 @@
 </script>
 
 <style scoped>
+  #home {
+    padding-top: 44px;
+  }
   .home-nav {
     background-color: var(--color-tint);
     color: white;
+
+    position: fixed;
+    left: 0;
+    right: 0;
+  }
+
+  .tab-control {
+    position: sticky;
+    top: 44px
   }
 </style>
